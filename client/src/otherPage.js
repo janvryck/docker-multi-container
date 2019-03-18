885775cf324497353311b712be3828b179b4c46d
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-03-18 13:55:45 +0100

    Revert "Multi: err on the safe side, use instructor files"
    This reverts commit 910fd6a67b4147fcdd31a63b493539ded914c9d9.

--- client/src/otherPage.js
+++ client/src/otherPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => {
-  return (
-    <div>
-      Im some other page
-      <Link to="/">Go back to home page!</Link>
-    </div>
-  );
+    return (
+        <div>
+            Other page
+            <Link to="/">To Home</Link>
+        </div>
+    );
 };
